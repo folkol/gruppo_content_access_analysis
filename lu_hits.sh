694356f3eb7fca69fc0a864dc9a1ed0aff95b8c3
Matte <mattias.johansson@atex.com>
2015-08-28 10:52:03 +0200

    Some refactoring

--- lu_hits.sh
+++ lu_hits.sh
@@ -1,4 +1,5 @@
-grep 'INFO: Request URI:' $1 \
+cat $1 \
+    | grep 'INFO: Request URI:' \
     | awk '{print $7, $4}' \
     | sed -E 's/lookups=([0-9]+)/\1/g' \
     | q "select sum(c1), c2 from - group by c2" \
